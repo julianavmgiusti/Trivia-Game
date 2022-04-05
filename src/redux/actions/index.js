295ba3fe2d97939a3750aa1d36fa1a056a8300d4
Juliana Giusti <juliana.vmelo@gmail.com>
2022-04-05 16:49:29 -0300

    req 2

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,4 +1,27 @@
-export default login = (value) => ({
+export const LOGIN = 'LOGIN';
+export const GET_TOKEN = 'GET_TOKEN';
+
+export const login = (value) => ({
   type: 'LOGIN',
-  value,
+  payload: value,
 });
+
+export const getToken = (payload) => ({
+  type: GET_TOKEN,
+  value: payload,
+});
+
+export const fetchTokenThunk = () => {
+  const URL = 'https://opentdb.com/api_token.php?command=request';
+  return async () => {
+    try {
+      const respToken = await fetch(URL);
+      const dataToken = await respToken.json();
+      const { token } = dataToken;
+      console.log(dataToken);
+      localStorage.setItem('token', token);
+    } catch (error) {
+      return error.message;
+    }
+  };
+};

3c822d5e354a9a1541e0e0ecf07b5fcfd15cf644
hellenribas <tchwtrabalho@gmail.com>
2022-04-06 16:13:01 -0300

    requisito 5 e 6

--- src/components/Play.js
+++ src/components/Play.js
@@ -2,87 +2,17 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import propTypes from 'prop-types';
 import Header from './Header';
+import Game from './Game';
 import { requestApi, fetchTokenThunk } from '../redux/actions';
 
-const numThree = 3;
 class Play extends Component {
-  constructor() {
-    super();
-    this.state = {
-      count: 0,
-    };
-  }
-
-  componentDidMount() {
-    console.log('entrou');
-    this.verify();
-  }
-
-  verify = () => {
-    const { token, gameApi, fetchToken } = this.props;
-    console.log(token);
-    gameApi(token);
-    const { resultsApi } = this.props;
-    if (resultsApi.response_code === numThree) {
-      fetchToken();
-      gameApi(token);
-    }
-  }
-
-  // https://www.ngeeks.com/javascript-avanzado-desordenar-un-array/
-
-  handleAlternatives = (count) => {
-    const { resultsApi: { results } } = this.props;
-    const meio = 0.5;
-    const array = [...results[count].incorrect_answers, results[count].correct_answer];
-    return array.map((answer, i) => {
-      if (i === array.length - 1) {
-        return (
-          <button
-            type="button"
-            data-testid="correct-answer"
-            key={ i }
-          >
-            { answer }
-          </button>);
-      }
-      return (
-        <button data-testid={ `incorrect-answer-${i}` } type="button" key={ i }>
-          { answer }
-        </button>
-      );
-    })
-      .sort(() => Math.random() - meio);
-  }
-
   render() {
-    const { count } = this.state;
     const { resultsApi: { results } } = this.props;
-    console.log(results);
+
     return (
       <div>
         <Header />
-        { results !== undefined
-        && (
-          <section>
-            <h1 data-testid="question-category">
-              {
-                results[count].category
-              }
-
-            </h1>
-            <p data-testid="question-text">
-              {
-                results[count].question
-              }
-
-            </p>
-            <section>
-              <div data-testid="answer-options">
-                { this.handleAlternatives(count) }
-              </div>
-            </section>
-          </section>)}
+        <Game results={ results } />
       </div>
     );
   }

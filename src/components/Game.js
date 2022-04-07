147bf35765647f29b497ec729573ede40f57ce46
hellenribas <tchwtrabalho@gmail.com>
2022-04-07 16:55:37 -0300

    requisito 8

--- src/components/Game.js
+++ src/components/Game.js
@@ -8,24 +8,24 @@ export default class Game extends Component {
       count: 0,
       isDisabled: true,
       timer: 30,
-      rightAnswer: '',
       answers: [],
+      rightAnswer: '',
     };
   }
 
   componentDidMount = () => {
     const { count } = this.state;
+    const { results } = this.props;
     this.handleInterval();
-    this.handleAlternatives(count);
-    // const buttons = this.handleAlternatives(count);
-    // console.log(buttons);
-    // this.setState({ button: buttons }, () => {
-    //   const { button } = this.state;
-    //   console.log(button);
-    // });
+    const buttons = this.handleAlternatives(count);
+    this.setState({
+      answers: buttons,
+      rightAnswer: results[count].correct_answer,
+    });
   }
 
   handleClick = () => {
+    console.log('clicou');
     const correct = document.querySelector('.correct');
     const wrong = document.getElementsByClassName('wrong');
     correct.style.border = '3px solid rgb(6, 240, 15)';
@@ -46,61 +46,51 @@ export default class Game extends Component {
       this.handleClick();
     }, thirtySeconds);
 
-    setInterval(() => {
-      this.setState((prevState) => ({
-        timer: prevState.timer - 1,
-      }));
+    const count = setInterval(() => {
+      this.setState((prevState) => {
+        if (prevState.timer < 1) clearInterval(count);
+        return { timer: prevState.timer - 1,
+        };
+      });
     }, one);
   }
 
   handleAlternatives = (count) => {
     const { results } = this.props;
-    console.log(results);
-    const { isDisabled } = this.state;
     const meio = 0.5;
-    const array = [...results[count].incorrect_answers, results[count].correct_answer];
-    const answers = array.sort(() => Math.random() - meio);
-    this.setState({
-      rightAnswer: results[count].correct_answer,
-      answers,
-    });
-  }
-
-  teste = () => {
-
+    const array = [...results[0].incorrect_answers, results[count].correct_answer];
+    return array
+      .sort(() => Math.random() - meio);
   }
 
   render() {
-    const { count, timer, answers, rightAnswer } = this.state;
-    // console.log(button);
+    const { count, timer, answers, rightAnswer, isDisabled } = this.state;
+    console.log(answers);
     const { results } = this.props;
     return (
       <div>
         { results && (
-
           <section>
             <h1 data-testid="question-category">
               {
-
                 results[count].category
               }
-
             </h1>
             <p data-testid="question-text">
               {
                 results[count].question
               }
-
             </p>
             <section>
               <div data-testid="answer-options">
-                { answers
-                  .map((answer, i) => {
+                {
+                  answers.map((answer, i) => {
                     if (answer === rightAnswer) {
                       return (
                         <button
                           type="button"
                           data-testid="correct-answer"
+                          id="correct"
                           key={ i }
                           className="correct"
                           onClick={ this.handleClick }
@@ -121,7 +111,8 @@ export default class Game extends Component {
                         { answer }
                       </button>
                     );
-                  }) }
+                  })
+                }
               </div>
             </section>
             <section>
